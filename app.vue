109d437b66f889448af886ced1db3e45054396af
Mishu <adri.alvrzt@gmail.com>
2023-05-17 20:05:57 +0200

    fixed image

--- app.vue
+++ app.vue
@@ -1,24 +1,28 @@
 <script setup></script>
 
-
 <template>
   <div class="flex flex-col justify-between min-h-screen ...">
-    <div class="pl-12 mb-4 text-5xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl drop-shadow-xl flex">
+    <div
+      class="pl-12 md:pl-12 sm:pl-12 xl:pl-16 pt-16 mb-4 text-5xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl drop-shadow-xl flex"
+    >
       <strong>
-        <br />
-          <div class="hover:text-pink-200">
-            <h1>Hi! I'm Lucía ♡</h1>
-          </div>
+        <div class="hover:text-pink-200">
+          <h1>Hi! I'm Lucía ♡</h1>
+        </div>
         <br />
         <u>
-          <div class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300">
-            <a class="underline decoration-pink-300/50 hover:text-pink-200 flex"
+          <div
+            class="transition ease-in-out delay-150 hover:-translate-y-1 duration-300"
+          >
+            <a
+              class="underline decoration-pink-300/50 hover:text-pink-200 flex"
               href="https://mishu.dev"
-              >mishu.dev</a>
+              >mishu.dev</a
+            >
           </div>
         </u>
         <p class="hover:text-pink-200">Coming Soon! ♡</p>
-        </strong>
+      </strong>
       <br />
       <!-- <button
         disabled
@@ -45,17 +49,21 @@
         Loading...
       </button> -->
     </div>
-    <div class=" lg:text-6xl xl:text-7xl 2xl:text-8xl">
+    <div class="lg:text-6xl xl:text-7xl 2xl:text-8xl">
       <img
         src="/owo.png"
         alt="mishu.dev"
-        class="absolute bottom-0 right-0 h-98 w-98 md:h-100 md:w-100 sm:h-96 sm:w-96 lg:h-98 lg:w-98 xl:h-98 xl:w-98"
+        class="absolute bottom-0 right-0 h-97 w-97 sm:h-98 sm:w-98 md:h-96 md:w-96 lg:h-97 lg:w-97 xl:h-98 xl:w-98 2xl:h-100 2xl:w-100"
       />
     </div>
 
     <footer class="body-font">
-      <div class="container px-5 py-8 mx-auto flex items-center flex-row flex-col">
-        <p class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4 text-white hover:text-pink-200">
+      <div
+        class="container px-5 py-8 mx-auto flex items-center flex-row flex-col"
+      >
+        <p
+          class="text-sm opacity-70 sm:py-2 sm:mt-0 mt-4 text-white hover:text-pink-200"
+        >
           © 2023 Lucía Álvarez
         </p>
       </div>
@@ -88,14 +96,12 @@
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡀⠀⠀⠀⠀⢀⣤⣶⠆⢀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣄⠀⠀⠀⠙⣛⣡⣶⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
--->
-
-</template>
+--></template>
 
 <style>
-/* * {
+* {
   border: 1px solid red;
-} */
+}
 
 @font-face {
   font-family: "RockoUltraFLF";
