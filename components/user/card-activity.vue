8c3f359cbb17727e0f8120698ca9097870777577
Mishu <adri.alvrzt@gmail.com>
2023-06-19 17:31:12 +0200

    Activity display

--- components/user/card-activity.vue
+++ components/user/card-activity.vue
@@ -1,65 +1,61 @@
 <template>
-	<div class="mb-3">
+	<div class="">
 		<h2 class="mb-2 text-xs font-bold uppercase leading-4 text-zinc-700 dark:text-slate-300">Playing a game</h2>
 
 		<!-- bodyNormal -->
-		<div class="flex items-center">
-			<!-- assets -->
-			<div class="relative self-start">
-				<!-- This image only will show if there is data.application_id -->
-				<img
-					v-if="data.application_id"
-					:src="`https://cdn.discordapp.com/app-assets/${data.application_id}/${data.assets!.large_image}.png`"
-					:alt="data.assets!.large_text"
-					width="60"
-					height="60"
-					draggable="false"
-					class="block rounded-lg object-cover"
-					:class="{ 'large-mask': data.assets!.small_text }"
-				/>
-				<img
-					:src="`https://dcdn.dstn.to/app-icons/${gameID}.png`"
-					:alt="gameName"
-					width="60"
-					height="60"
-					draggable="false"
-					class="block rounded-lg object-cover"
-				/>
-				<!-- This image only will show if there isn't data.application_id -->
-				<img
-					v-if="!data.application_id"
-					:src="imageUrl"
-					:alt="`spotify song`"
-					width="60"
-					height="60"
-					class="block rounded-lg object-cover"
-					:class="{ 'large-mask': data.assets!.small_text }"
-				/>
-				<!-- This image only will show if there is a small image of the activity -->
-				<img
-					v-if="data.assets!.small_text"
-					:src="`https://cdn.discordapp.com/app-assets/${data.application_id}/${data.assets!.small_image}.png`"
-					:alt="data.assets!.small_text"
-					width="20"
-					height="20"
-					draggable="false"
-					class="absolute -bottom-1 -right-1 rounded-full"
-				/>
-			</div>
-
-			<!-- content -->
-			<div class="ml-2.5 flex-auto overflow-hidden">
-				<div class="block overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
-					{{ data.name }}
-				</div>
-				<div v-if="data.details" class="block overflow-hidden text-ellipsis whitespace-nowrap">
-					{{ data.details }}
-				</div>
-				<div v-if="data.state" class="block overflow-hidden text-ellipsis whitespace-nowrap">
-					{{ data.state }}
-				</div>
-				<div v-if="elapsed" class="block overflow-hidden text-ellipsis whitespace-nowrap">{{ elapsed }} elapsed</div>
-			</div>
+		<div class="flex">
+			<ul>
+				<li v-for="(activity, index) in activities" :key="activity.id">
+					<template v-if="index !== 0">
+						<!-- assets -->
+						<div class="flex flex-col gap-y-8">
+							<div class="mb-4 flex items-center gap-x-2">
+								<div>
+									<!-- spotify -->
+									<img
+										v-if="activity.name == 'Spotify'"
+										:src="imageUrl"
+										alt="valorant"
+										draggable="false"
+										class="h-[60px] w-[60px] rounded-lg object-cover"
+									/>
+									<!-- non-spotify applicationn with assets-->
+									<img
+										v-if="activity.name != 'Spotify' && activity.assets"
+										:src="`https://cdn.discordapp.com/app-assets/${activity.application_id}/${activity.assets!.large_image}.png`"
+										:alt="activity.assets!.large_text"
+										draggable="false"
+										class="h-[60px] w-[60px] rounded-lg object-cover"
+									/>
+									<!-- non-spotify applications and no assets-->
+									<img
+										v-if="activity.name != 'Spotify' && !activity.assets"
+										:src="`https://dcdn.dstn.to/app-icons/${activity.application_id}.png`"
+										:alt="activity.name"
+										draggable="false"
+										class="h-[60px] w-[60px] rounded-lg object-cover"
+									/>
+								</div>
+								<div>
+									<!-- content -->
+									<div class="ml-2.5 flex-auto overflow-hidden">
+										<div class="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
+											{{ activity.name }}
+										</div>
+										<div v-if="data.details" class="overflow-hidden text-ellipsis whitespace-nowrap">
+											{{ activity.details }}
+										</div>
+										<div v-if="data.state" class="overflow-hidden text-ellipsis whitespace-nowrap">
+											{{ activity.state }}
+										</div>
+										<div v-if="elapsed" class="overflow-hidden text-ellipsis whitespace-nowrap">{{ elapsed }} elapsed</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</template>
+				</li>
+			</ul>
 		</div>
 
 		<!-- buttons -->
@@ -86,9 +82,11 @@ const minuteAsMilliseconds = secondAsMilliseconds * 60;
 const hourAsMilliseconds = minuteAsMilliseconds * 60;
 
 const props = defineProps<{ data: GatewayActivity }>();
+const activities = ref<GatewayActivity[]>([]);
 const imageUrl = ref('');
 const gameName = ref('');
 const gameID = ref('');
+
 onMounted(async () => {
 	try {
 		const response = await fetch('https://api.lanyard.rest/v1/users/300969054649450496');
@@ -96,11 +94,12 @@ onMounted(async () => {
 		imageUrl.value = lyndata.data.spotify.album_art_url;
 		gameName.value = lyndata.data.activities.name;
 		gameID.value = lyndata.data.activities.application_id;
+		activities.value = lyndata.data.activities;
 	} catch (error) {
 		console.error('Error fetching data:', error);
 	}
 });
-console.log(`https://dcdn.dstn.to/app-icons/${gameID}.png`, gameID);
+
 const elapsed = useState<string | null>('user-card-activity-elapsed', computeElapsed);
 window.setInterval(() => (elapsed.value = computeElapsed()), 1000);
 
