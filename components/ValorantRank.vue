721ef2626d63db9c8d5b3282b4892dde243cb9f0
Lucía <124211865+lucialv@users.noreply.github.com>
2023-11-14 11:45:13 +0100

    Update ValorantRank.vue

--- components/ValorantRank.vue
+++ components/ValorantRank.vue
@@ -11,7 +11,7 @@
 					></div>
 				</h1>
 			</div>
-			<div class="mt-10 rounded-lg bg-gradient-to-tl from-blue-500 via-blue-400 to-blue-200 p-32 md:items-center lg:flex">
+			<div class="mt-10 rounded-lg bg-gradient-to-tl from-blue-500 via-blue-400 to-blue-200 p-16 lg:p-32 md:items-center lg:flex">
 				<div class="lg:mr-16">
 					<div class="mb-4 mt-8 flex items-center">
 						<a
